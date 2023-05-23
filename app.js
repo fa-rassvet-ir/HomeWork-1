d54a6c4e2b3439448f3d4734be55752d396a8c64
Rassvet <talmazuulu2003@gmail.com>
2023-05-23 20:08:39 +0600

    Первая домашнее задание.

---
+++ app.js
@@ -0,0 +1,11 @@
+var myPassword = prompt("Введите пароль")
+
+if (myPassword.toLowerCase() === "пароль") {
+    console.log('Пароль введен правильно');
+}
+
+// alert(myLogin)
+var myLogin = prompt("Введите ЛОГИН")
+if (myLogin.toUpperCase() === "ЛОГИН") {
+   console.log('Добро пожаловать'); 
+}
